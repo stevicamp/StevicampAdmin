b19ff27d2531eeb80e2b4cfd084f55dc56cb3018
stevicamp <archangel75@abv.bg>
2026-02-07 15:24:48 +0200

    ok

--- static/js/main.js
+++ static/js/main.js
@@ -44,7 +44,7 @@ export function router(e) {
     // e = event || window.event;
     e.preventDefault(); // Prevent deafult behavior don't follow the link
     const url = new URL(e.target.href);
-    const path = url.pathname; // Only local path since there is problem with blogger and the <base> url.
+    const path = url.pathname + url.search;; // Only local path since there is problem with blogger and the <base> url.
     window.history.pushState(null, null, window.location.origin + path); // Add the url to the history api of js so we can navigate back and forth with the browser buttons
     handleLocation();
 }
